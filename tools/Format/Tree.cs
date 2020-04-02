630704b9e84f79056bbd1821274341f0f7037153
Vincent Hoogendoorn <dev@vincenth.net>
2020-04-02 10:32:03 +0200

    Parse and render source scopes

---
+++ tools/Format/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpMarkupTools
+{
+    public class Tree<T> where T : class
+    {
+        Tree<T> parent;
+        readonly List<Tree<T>> children = new List<Tree<T>>();
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            Value = value;
+            Add(children);
+        }
+
+        public T Value { get; set; }
+
+        public Tree<T> Parent 
+        {
+            get => parent;
+            
+            private set
+            {
+                if (object.ReferenceEquals(value, parent)) return;
+                if (value != null && parent != null) throw new InvalidOperationException($"Node {Value} already has a parent");
+                parent = value;
+                parent.children.Add(this);
+            }
+        }
+
+        public IReadOnlyList<Tree<T>> Children => children.AsReadOnly();
+
+        public void Add(params Tree<T>[] children)
+        {
+            foreach (var child in children) child.Parent = this;
+        }
+    }
+}
--- tools/Tests/Formatter.cs
+++ tools/Tests/Formatter.cs
@@ -13,9 +13,31 @@ namespace CSharpMarkupTools.Tests
         public void CorrectFormatRemainsIdentical()
         {
             string source = @"
+using CSharpForMarkup;
+using Xamarin.Forms;
+
+namespace CSharpForMarkupExample.Views.Pages
+{
+    public partial class AnimatedPage : BaseContentPage
+    {
+        void Build() {
+            Title = ""AnimatedPage"";
+
+            Content = new StackLayout { Children = {
+                new Label { Text = $""Animation example {1}"" } .FontSize (32) .CenterH (),
+                new Label { Text = ""to illustrate""     } .FontSize (32) .CenterH (),
+                new Label { Text = ""separation""        } .FontSize (32) .CenterH (),
+                new Label { Text = ""of""                } .FontSize (32) .CenterH (),
+                new Label { Text = ""UI Markup"", TextColor = Colors.Green.ToColor() } .FontSize (32) .Bold () .CenterH (),
+                new Label { Text = ""and""               } .FontSize (32) .CenterH (),
+                new Label { Text = ""UI Logic"" , TextColor = Colors.Red.ToColor()   } .FontSize (32) .Bold () .CenterH ()
+            } }.Assign(out animatedStackLayout);
+        }
+    }
+}
 ";
-            var formatter = new CSharpMarkupTools.Formatter(new CSharpMarkupTools.Formatter.Settings { FormatWhiteSpace = true, MaxLineLength = 80 });
-            Assert.That(source, Is.EqualTo(formatter.Format(source)));
+            var formatter = new CSharpMarkupTools.Formatter(source, new CSharpMarkupTools.Formatter.Settings { FormatWhiteSpace = true, MaxLineLength = 80 });
+            Assert.That(source, Is.EqualTo(formatter.Format()));
         }
     }
 }
