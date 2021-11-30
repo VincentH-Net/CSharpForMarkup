06dc9887bebf04b8effb8d8fe0f851ea8ced7939
VincentH.NET <dev@vincenth.net>
2021-11-30 06:49:17 +0100

    Fix for NuGet restore error when package is not built locally

--- src/CSharpMarkup.WinUI.Examples/Examples.Shared/Core/MarkupExtensions.cs
+++ src/CSharpMarkup.WinUI.Examples/Examples.Shared/Core/MarkupExtensions.cs
@@ -5,12 +5,13 @@ namespace WinUICsMarkupExamples;
 
 internal static class MarkupExtensions
 {
+    // TODO: Replace with MetadataUpdateHandler attribute after that works for .NET hot reload in WinUI
     internal static UI.UIElement WithHotReloadButton(this UIElement content) =>
-#if WINDOWS && DEBUG // TODO: Replace with MetadataUpdateHandler attribute after that works for .NET hot reload in WinUI
+#if WINDOWS && DEBUG
         Grid(
             content.UI,
 
-            Button("\U0001F525").Top().Right().Command(new RelayCommand(App.Current.BuildWindowContent))
+            Button("\U0001F525") .Top() .Right() .Command (new RelayCommand(App.Current.BuildWindowContent))
         )
 #else
         content
