dc8afe3037099c0031509407b1fd0d3701bb4a54
VincentH.NET <dev@vincenth.net>
2021-12-06 15:25:53 +0100

    Add API required for codegen, WinUI version

---
+++ src/CSharpMarkup.Wpf/DefaultBindProperties.cs
@@ -0,0 +1,45 @@
+﻿using Controls = Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.UI.Markup
+{
+    public interface IDefaultBindProperty // TODO: Generate this automatically if only one possibility
+    {
+        Xaml.DependencyProperty DefaultBindProperty { get; }
+    }
+
+    public interface IDefaultBindCommandProperties // TODO: Generate this automatically if only one possibility, and from helpers where specified (if needed) ? only if there is a property <name>CommandParameterProperty and <name>CommandProperty?
+    {
+        Xaml.DependencyProperty DefaultBindCommandProperty { get; }
+        Xaml.DependencyProperty DefaultBindCommandParameterProperty { get; }
+    }
+
+    public partial class MenuFlyoutItem : IDefaultBindCommandProperties
+    {
+        public Xaml.DependencyProperty DefaultBindCommandProperty => Controls.MenuFlyoutItem.CommandProperty;
+        public Xaml.DependencyProperty DefaultBindCommandParameterProperty => Controls.MenuFlyoutItem.CommandParameterProperty;
+    }
+
+    public partial class SplitButton : IDefaultBindCommandProperties
+    {
+        public Xaml.DependencyProperty DefaultBindCommandProperty => Controls.SplitButton.CommandProperty;
+        public Xaml.DependencyProperty DefaultBindCommandParameterProperty => Controls.SplitButton.CommandParameterProperty;
+    }
+
+    public partial class SwipeItem : IDefaultBindCommandProperties
+    {
+        public Xaml.DependencyProperty DefaultBindCommandProperty => Controls.SwipeItem.CommandProperty;
+        public Xaml.DependencyProperty DefaultBindCommandParameterProperty => Controls.SwipeItem.CommandParameterProperty;
+    }
+
+    public partial class TabView : IDefaultBindCommandProperties
+    {
+        public Xaml.DependencyProperty DefaultBindCommandProperty => Controls.TabView.AddTabButtonCommandProperty;
+        public Xaml.DependencyProperty DefaultBindCommandParameterProperty => Controls.TabView.AddTabButtonCommandParameterProperty;
+    }
+
+    public partial class ButtonBase : IDefaultBindCommandProperties
+    {
+        public Xaml.DependencyProperty DefaultBindCommandProperty => Controls.Primitives.ButtonBase.CommandProperty;
+        public Xaml.DependencyProperty DefaultBindCommandParameterProperty => Controls.Primitives.ButtonBase.CommandParameterProperty;
+    }
+}
