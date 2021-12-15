54606b089ef8348ae3da6762adb81276a42d34b4
VincentH.NET <dev@vincenth.net>
2021-12-15 14:24:29 +0100

    WPF - specify default bindable properties for all suitable controls in namespaces System.Windows.Controls and System.Windows.Controls.Primitives

--- src/CSharpMarkup.Wpf/_Helpers.cs
+++ src/CSharpMarkup.Wpf/_Helpers.cs
@@ -19,31 +19,41 @@ namespace CSharpMarkup.Wpf
     {
         #if GENERATE
         /// <summary>Used by codegen to generate a <see cref="IDefaultBindProperty"/> implementation on markup types. Not used at runtime.</summary>
-        /// <remarks>Types must be fully specified for codegen to work</remarks>
+        /// <remarks>
+        /// - Types must be fully specified for codegen to work
+        /// - A default property for a derived type hides any default properties of it's base types
+        /// </remarks>
         static System.Windows.DependencyProperty[] DefaultBindProperties => new System.Windows.DependencyProperty[]
         {
+            System.Windows.Controls.Calendar.SelectedDateProperty,
             System.Windows.Controls.ColumnDefinition.WidthProperty,
             System.Windows.Controls.ContentControl.ContentProperty,
-            System.Windows.Controls.ContentPresenter.ContentProperty,
-
+            System.Windows.Controls.ContentPresenter.ContentProperty, // TODO: or remove and select more useful defaults for derived controls?
+            System.Windows.Controls.DatePicker.SelectedDateProperty,
+            System.Windows.Controls.Expander.IsExpandedProperty,
+            System.Windows.Controls.FlowDocumentReader.DocumentProperty,
+            System.Windows.Controls.FlowDocumentScrollViewer.DocumentProperty,
             System.Windows.Controls.Image.SourceProperty,
+            System.Windows.Controls.InkPresenter.StrokesProperty,
             System.Windows.Controls.ItemsControl.ItemsSourceProperty,
-
+            System.Windows.Controls.KeyTipControl.TextProperty,
+            System.Windows.Controls.Label.TargetProperty,
             System.Windows.Controls.MediaElement.SourceProperty,
-
-            System.Windows.Controls.Primitives.ButtonBase.CommandProperty,
-
-            System.Windows.Controls.Primitives.RangeBase.ValueProperty,
-
-            System.Windows.Controls.Primitives.ToggleButton.IsCheckedProperty,
-
+            System.Windows.Controls.Page.TitleProperty,
             System.Windows.Controls.RowDefinition.HeightProperty,
-
+            System.Windows.Controls.SoundPlayerAction.SourceProperty,
+            System.Windows.Controls.SpellCheck.IsEnabledProperty,
             System.Windows.Controls.TextBlock.TextProperty,
             System.Windows.Controls.TextBox.TextProperty,
+            System.Windows.Controls.Viewport3D.ChildrenProperty,
 
-            System.Windows.Controls.TreeView.ItemsSourceProperty,
-            System.Windows.Controls.TreeViewItem.ItemsSourceProperty,
+            System.Windows.Controls.Primitives.ButtonBase.CommandProperty,
+            System.Windows.Controls.Primitives.DocumentViewerBase.DocumentProperty,
+            System.Windows.Controls.Primitives.GridViewRowPresenterBase.ColumnsProperty,
+            System.Windows.Controls.Primitives.RangeBase.ValueProperty,
+            System.Windows.Controls.Primitives.SelectiveScrollingGrid.SelectiveScrollingOrientationProperty,
+            System.Windows.Controls.Primitives.ToggleButton.IsCheckedProperty,
+            System.Windows.Controls.Primitives.Track.ValueProperty,
 
             // TODO: Complete items after last one above + from subnamespaces Documents, Input, Media, Media.Animation, Shapes
 
