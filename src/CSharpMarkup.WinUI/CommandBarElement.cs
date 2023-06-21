030e5ca1b3d90ea72a26e8bb72945f896c364b5a
VincentH.NET <dev@vincenth.net>
2023-06-21 09:34:18 +0200

    source cleanup for support of collection type ContentProperty

---
+++ src/CSharpMarkup.WinUI/CommandBarElement.cs
@@ -0,0 +1,21 @@
+﻿using Xaml = Microsoft.UI.Xaml;
+
+namespace CSharpMarkup.WinUI;
+
+public class CommandBarElement : Xaml.Controls.ICommandBarElement
+{
+    readonly Xaml.Controls.ICommandBarElement element;
+
+    #region ICommandBarElement implementation
+    public int DynamicOverflowOrder { get => element.DynamicOverflowOrder; set => element.DynamicOverflowOrder = value; }
+    public bool IsCompact { get => element.IsCompact; set => element.IsCompact = value; }
+    public bool IsInOverflow => element.IsInOverflow;
+    #endregion
+
+    public static implicit operator CommandBarElement(AppBarButton e)           => new (e.UI);
+    public static implicit operator CommandBarElement(AppBarElementContainer e) => new (e.UI);
+    public static implicit operator CommandBarElement(AppBarSeparator e)        => new (e.UI);
+    public static implicit operator CommandBarElement(AppBarToggleButton e)     => new (e.UI);
+
+    public CommandBarElement(Xaml.Controls.ICommandBarElement element) => this.element = element;
+}
---
+++ src/CSharpMarkup.WinUI/MenuFlyoutItemBase.cs
@@ -0,0 +1,8 @@
+﻿using Xaml = Microsoft.UI.Xaml;
+
+namespace CSharpMarkup.WinUI;
+
+public partial class MenuFlyoutItemBase
+{
+    public static implicit operator Xaml.Controls.MenuFlyoutItemBase(MenuFlyoutItemBase item) => item.UI;
+}
