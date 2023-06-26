2108c8c50bf9a4eb6d312fc5b67fdd57a24d9a8a
VincentH.NET <dev@vincenth.net>
2023-06-26 09:23:20 +0200

    fix for ambiguous implicit conversion error when using classes derived from MenuFlyoutItemBase

--- src/CSharpMarkup.WinUI/MenuFlyoutItemBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using Xaml = Microsoft.UI.Xaml;
-
-namespace CSharpMarkup.WinUI;
-
-public partial class MenuFlyoutItemBase
-{
-    public static implicit operator Xaml.Controls.MenuFlyoutItemBase(MenuFlyoutItemBase item) => item.UI;
-}
