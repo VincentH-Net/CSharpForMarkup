a2cc5564e09ddec335d6dcf0e29b97983d52501e
VincentH.NET <dev@vincenth.net>
2023-09-27 13:37:18 +0200

    add NavigationBar convenience helper for Uno.Toolit

---
+++ src/CSharpMarkup.WinUI.Uno.Toolkit/NavigationBar.cs
@@ -0,0 +1,14 @@
+﻿using Xaml = Microsoft.UI.Xaml;
+
+namespace CSharpMarkup.WinUI.Uno.Toolkit
+{
+    public static partial class Helpers {
+        /// <summary>Create a <see cref="UnoToolkitUI.NavigationBar"/> with <paramref name="Content"/> </summary>
+        public static NavigationBar NavigationBar(object Content, params CSharpMarkup.WinUI.CommandBarElement[] PrimaryCommands)
+        {
+            var bar = NavigationBar(PrimaryCommands);
+            bar.UI.Content = Content;
+            return bar;
+        }
+    }
+}
