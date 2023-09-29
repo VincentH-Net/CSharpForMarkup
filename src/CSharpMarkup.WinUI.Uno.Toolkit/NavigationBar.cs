a02d8cef3c79802b83fcd23eb47baec99cf36670
VincentH.NET <dev@vincenth.net>
2023-09-29 13:25:22 +0200

    support UIObject in Uno Toolkit NavigationBar helper

--- src/CSharpMarkup.WinUI.Uno.Toolkit/NavigationBar.cs
+++ src/CSharpMarkup.WinUI.Uno.Toolkit/NavigationBar.cs
@@ -1,13 +1,13 @@
-﻿using Xaml = Microsoft.UI.Xaml;
+﻿using UnoToolkitUI = Uno.Toolkit.UI;
 
 namespace CSharpMarkup.WinUI.Uno.Toolkit
 {
     public static partial class Helpers {
         /// <summary>Create a <see cref="UnoToolkitUI.NavigationBar"/> with <paramref name="Content"/> </summary>
-        public static NavigationBar NavigationBar(object Content, params CSharpMarkup.WinUI.CommandBarElement[] PrimaryCommands)
+        public static NavigationBar NavigationBar(UIObject Content, params CommandBarElement[] PrimaryCommands)
         {
             var bar = NavigationBar(PrimaryCommands);
-            bar.UI.Content = Content;
+            bar.UI.Content = Content.UI;
             return bar;
         }
     }
