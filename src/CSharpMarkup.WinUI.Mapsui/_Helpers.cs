8671282d850e9bae4d361ffa6981ddabb164b747
Vincent Hoogendoorn <dev@vincenth.net>
2025-07-10 19:08:08 +0200

    add CSharpMarkup.WinUI.Mapsui with Mapsui 5.0 support, incl generated code

---
+++ src/CSharpMarkup.WinUI.Mapsui/_Helpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xaml = Microsoft.UI.Xaml;
+#if GENERATE
+using CSharpMarkup.Generate.WinUI;
+using Controls = Microsoft.UI.Xaml.Controls;
+
+[assembly: MarkupHelpers(
+    markupHelpersType: typeof(CSharpMarkup.WinUI.Mapsui.Helpers)
+)]
+#endif
+
+namespace CSharpMarkup.WinUI.Mapsui
+{
+    public static partial class Helpers
+    {
+
+#if GENERATE
+        /// <summary>Used by codegen to generate a <see cref="IDefaultBindProperty"/> implementation on markup types. Not used at runtime.</summary>
+        /// <remarks>Types must be fully specified for codegen to work</remarks>
+        static Xaml.DependencyProperty[] DefaultBindProperties => new Xaml.DependencyProperty[]
+        {
+            // TODO: e.g. Mapsui...Property,
+        };
+
+        /// <summary>
+        /// Used by codegen to identify which assemblies to scan for UI types. 
+        /// Markup types will be generated for these UI types (e.g. controls).
+        /// </summary>
+        static Type[] UIAssemblies => new Type[] // Assemblies in UI NuGets that contain UI types
+        {
+            typeof(global::Mapsui.UI.WinUI.MapControl)
+        };
+#endif
+    }
+}
