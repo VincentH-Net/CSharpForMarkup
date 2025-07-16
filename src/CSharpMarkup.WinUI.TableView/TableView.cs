0e1f79cdb0f015ca2b0aa4aead05181b38a6cf3a
Vincent Hoogendoorn <dev@vincenth.net>
2025-07-16 18:32:04 +0200

    add .TvItemsSource() extension method for TableView to avoid ambiguity with the .ItemsSource() extension method

---
+++ src/CSharpMarkup.WinUI.TableView/TableView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using TvUI = WinUI.TableView;
+
+namespace CSharpMarkup.WinUI.TableView
+{
+    public static partial class TableViewExtensions2
+    {
+        /// <summary>Bind (or set enum value of) <see cref="TvUI.TableView.ItemsSource"/></summary>
+        /// <remarks>Use this method to avoid ambiguity of the .ItemsSource() extension method</remarks>
+        public static DependencyProperty<TTarget, IList?> TvItemsSource<TTarget>(this TTarget target) where TTarget : TableView
+        => DependencyProperty<TTarget, IList?>.Get(target, TvUI.TableView.ItemsSourceProperty);
+    }
+}
